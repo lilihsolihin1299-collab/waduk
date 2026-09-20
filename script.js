75bf63e46ec9818f8185615885ec779080ddbd07
lilihsolihin1299-collab <lilihsolihin1299@gmail.com>
2026-09-21 00:14:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* CONFIGURATION */
 const CONFIG = {
   smartlinkUrl: "https://pimplehardnesscarnivorous.com/x7zgrhfuix?key=1b597d65e3fa566d005f9a1ec5e6d385",
-  popunderUrl: "<script src="https://pimplehardnesscarnivorous.com/7d/c0/f5/7dc0f51577c92b0e7166a7adf63b7a86.js"></script>",
+  popunderUrl: "",
   adCooldown: 15000
 };
 
